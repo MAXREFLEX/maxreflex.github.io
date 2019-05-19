80cc45ea504eb1c3b1f722bc4052d7d164e92a52
MAXREFLEX <maxreflex2012@yandex.ru>
2019-05-19 14:15:20 +0400

    0

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,16 @@
-[].forEach.call(document.querySelectorAll('[data-src]'), function (img) {
-    img.setAttribute('src', img.getAttribute('data-src'));
+[].forEach.call(document.querySelectorAll('[data-href]'), function (img) {
+    img.setAttribute('href', img.getAttribute('data-href'));
     img.onload = function () {
-        img.removeAttribute('data-src');
+        img.removeAttribute('data-href');
+        lol();
     };
 });
+
+function lol() {
+    [].forEach.call(document.querySelectorAll('[data-src]'), function (img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function () {
+            img.removeAttribute('data-src');
+        };
+    });
+}
