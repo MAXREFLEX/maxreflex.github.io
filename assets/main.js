2c8c29b76f630391227a448aa8497cbaa7467cee
MAXREFLEX <maxreflex2012@yandex.ru>
2019-05-19 14:30:27 +0400

    0

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-[].forEach.call(document.querySelectorAll('[data-href]'), function (img) {
+[].forEach.call(document.querySelectorAll('link[data-href]'), function (img) {
     img.setAttribute('href', img.getAttribute('data-href'));
     img.onload = function () {
         img.removeAttribute('data-href');
@@ -7,10 +7,18 @@
 });
 
 function lol() {
-    [].forEach.call(document.querySelectorAll('[data-src]'), function (img) {
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
         img.setAttribute('src', img.getAttribute('data-src'));
         img.onload = function () {
             img.removeAttribute('data-src');
+            lal();
         };
+    });
+}
+
+function lal() {
+    [].forEach.call(document.querySelectorAll('script[data-src]'), function (img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.removeAttribute('data-src');
     });
 }
