c29abff619f05a3fd6e75db88b8f63ca2657c3f3
MAXREFLEX <maxreflex2012@yandex.ru>
2019-05-19 13:28:08 +0400

    0

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+[].forEach.call(document.querySelectorAll('[data-src]'), function (img) {
     img.setAttribute('src', img.getAttribute('data-src'));
     img.onload = function () {
         img.removeAttribute('data-src');
